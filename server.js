94486bdffbc69f6d8449c0584909a6d469d2b95d
lwaziT <toboselwazi9@gmail.com>
2022-05-17 13:24:49 +0200

    Modified structure

--- server.js
+++ server.js
@@ -7,3 +7,10 @@
 //Delete an object
 //Delete all objects
 //Test API
+//Methods       Url                 Actions
+//Get           api/tutorials       get all tutorials
+//Get           api/tutorials/:id   get tutorial by id
+//POST          api/tutorials       add new tutorial
+//PUT           api/tutorials/:id   Update tutorial by ID
+//DELETE        api/tutorials/:id   Remove tutorial by ID
+//DELETE        api/tutorials       Remove all tutorials
